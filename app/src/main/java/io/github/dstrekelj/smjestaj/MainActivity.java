268ac570801800292ffa3d08b506005c7719d43d
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2016-04-29 22:40:57 +0200

    Added prototype lodging adapter and updated MainActivity with an example of its use.

--- app/src/main/java/io/github/dstrekelj/smjestaj/MainActivity.java
+++ app/src/main/java/io/github/dstrekelj/smjestaj/MainActivity.java
@@ -4,32 +4,53 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
 
 import com.google.gson.Gson;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import io.github.dstrekelj.smjestaj.activities.ItemActivity;
+import io.github.dstrekelj.smjestaj.adapters.LodgingAdapter;
+import io.github.dstrekelj.smjestaj.models.LodgingModel;
+import io.github.dstrekelj.smjestaj.tasks.LodgingsJsonReaderAsyncTask;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LodgingsJsonReaderAsyncTask.ILodgingsJsonReader {
+
+    ListView lvItems;
+
+    ArrayList<LodgingModel> lodgingModelArrayList;
+    LodgingAdapter lodgingAdapter;
+
+    LodgingsJsonReaderAsyncTask lodgingsJsonReaderAsyncTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        InputStream stream = null;
-        try {
-            stream = getResources().getAssets().open("lodgings.json");
-            Scanner scanner = new Scanner(stream);
-            Log.d("json", scanner.next());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        lvItems = (ListView) findViewById(R.id.activity_main_items);
 
+        lodgingModelArrayList = new ArrayList<LodgingModel>();
+        lodgingAdapter = new LodgingAdapter(lodgingModelArrayList);
+        lvItems.setAdapter(lodgingAdapter);
+
+        lodgingsJsonReaderAsyncTask = new LodgingsJsonReaderAsyncTask(this);
+        lodgingsJsonReaderAsyncTask.execute("lodgings.json");
+
+        /*
         Intent i = new Intent(this, ItemActivity.class);
         startActivity(i);
+        */
+    }
+
+    @Override
+    public void onLodgingsJsonReaderPostExecute(ArrayList<LodgingModel> lodgingModelArrayList) {
+        Log.i("MainActivity", "onLodgingsJsonReaderPostExecute");
+        lodgingAdapter.add(lodgingModelArrayList.get(0));
+        lodgingAdapter.add(lodgingModelArrayList.get(1));
     }
 }
---
+++ app/src/main/java/io/github/dstrekelj/smjestaj/adapters/LodgingAdapter.java
@@ -0,0 +1,86 @@
+package io.github.dstrekelj.smjestaj.adapters;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import io.github.dstrekelj.smjestaj.R;
+import io.github.dstrekelj.smjestaj.models.LodgingModel;
+
+/**
+ * Created by Domagoj on 29.4.2016..
+ */
+public class LodgingAdapter extends BaseAdapter {
+
+    ArrayList<LodgingModel> lodgingModelArrayList;
+
+    public LodgingAdapter(ArrayList<LodgingModel> lodgingModelArrayList) {
+        this.lodgingModelArrayList = lodgingModelArrayList;
+    }
+
+    @Override
+    public int getCount() {
+        return lodgingModelArrayList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return lodgingModelArrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Log.d("LodgingAdapter", "getView");
+        ItemViewHolder itemViewHolder;
+        Context context = parent.getContext();
+
+        if (convertView == null) {
+            convertView = View.inflate(context, R.layout.activity_main_item, null);
+
+            itemViewHolder = new ItemViewHolder();
+
+            itemViewHolder.ivItemImage = (ImageView) convertView.findViewById(R.id.activity_main_item_image);
+            itemViewHolder.tvItemHeading = (TextView) convertView.findViewById(R.id.activity_main_item_heading);
+            itemViewHolder.tvItemBody = (TextView) convertView.findViewById(R.id.activity_main_item_body);
+
+            convertView.setTag(itemViewHolder);
+        } else {
+            itemViewHolder = (ItemViewHolder) convertView.getTag();
+        }
+
+        LodgingModel lodgingModel = this.lodgingModelArrayList.get(position);
+        String bodyText = lodgingModel.getAddress() + "\n" + lodgingModel.getPostcode() + " " + lodgingModel.getCity();
+
+        itemViewHolder.tvItemHeading.setText(lodgingModel.getName());
+        itemViewHolder.tvItemBody.setText(bodyText);
+
+        return convertView;
+    }
+
+    public void add(LodgingModel lodgingModel) {
+        this.lodgingModelArrayList.add(lodgingModel);
+        this.notifyDataSetChanged();
+    }
+
+    public void remove(int position) {
+        this.lodgingModelArrayList.remove(position);
+        this.notifyDataSetChanged();
+    }
+
+    static class ItemViewHolder {
+        private ImageView ivItemImage;
+        private TextView tvItemHeading;
+        private TextView tvItemBody;
+    }
+}
